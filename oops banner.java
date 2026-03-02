78f3b73b80ec976a9853857bb314a630ada9c3da
kallamahalakshmi24-rgb <kallamahalakshmi24@gmail.com>
2026-03-02 09:06:01 +0530

    Initial commit OOPS Banner App

---
+++ oops banner.java
@@ -0,0 +1,20 @@
+/**
+ * OOPSBannerApp
+ *
+ * This application prints the text "OOPS" to the console.
+ *
+ * @author K
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * Main method - Entry point of the application
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+
+        // Print OOPS to console
+        System.out.println("OOPS");
+    }
+}
