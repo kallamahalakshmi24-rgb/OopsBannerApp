4ea4b5987618651adb4e3de39842bc6391b1aad0
kallamahalakshmi24-rgb <kallamahalakshmi24@gmail.com>
2026-03-10 08:54:49 +0530

    UC5

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,33 +1,19 @@
 public class OOPSBannerApp {
     public static void main(String[] args) {
+
+        // Declaration + initialization + population together
         String[] banner = {
-
-                String.join(" ",
-                        " *** ", " *** ", "**** ", " ****"),
-
-                String.join(" ",
-                        "*   *", "*   *", "*   *", "*    "),
-
-                String.join(" ",
-                        "*   *", "*   *", "*   *", "*    "),
-
-                String.join(" ",
-                        "*   *", "*   *", "**** ", " *** "),
-
-                String.join(" ",
-                        "*   *", "*   *", "*    ", "    *"),
-
-                String.join(" ",
-                        "*   *", "*   *", "*    ", "*   *"),
-
-                String.join(" ",
-                        " *** ", " *** ", "*    ", " *** ")
+                String.join(" ", " *** ", " *** ", "**** ", " ****"),
+                String.join(" ", "*   *", "*   *", "*   *", "*    "),
+                String.join(" ", "*   *", "*   *", "*   *", "*    "),
+                String.join(" ", "*   *", "*   *", "**** ", " *** "),
+                String.join(" ", "*   *", "*   *", "*    ", "    *"),
+                String.join(" ", "*   *", "*   *", "*    ", "*   *"),
+                String.join(" ", " *** ", " *** ", "*    ", " *** ")
         };
 
-        // Loop to print banner
         for (String line : banner) {
             System.out.println(line);
         }
-
     }
 }
